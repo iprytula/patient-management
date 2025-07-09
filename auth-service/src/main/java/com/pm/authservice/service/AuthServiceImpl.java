bbd8b87d9f5e1d66c450c448cf942773c71f7fab
Iurii Prytula <iurii.prytula@gmail.com>
2025-07-09 12:05:12 +0200

    created login endpoint

---
+++ auth-service/src/main/java/com/pm/authservice/service/AuthServiceImpl.java
@@ -0,0 +1,31 @@
+package com.pm.authservice.service;
+
+import com.pm.authservice.dto.LoginRequestDTO;
+import com.pm.authservice.util.JwtUtil;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AuthServiceImpl implements AuthService {
+
+	private final UserService userService;
+	private final PasswordEncoder passwordEncoder;
+	private final JwtUtil jwtUtils;
+
+	public AuthServiceImpl(UserService userService, PasswordEncoder passwordEncoder, JwtUtil jwtUtils) {
+		this.jwtUtils = jwtUtils;
+		this.userService = userService;
+		this.passwordEncoder = passwordEncoder;
+	}
+
+	@Override
+	public Optional<String> authenticate(LoginRequestDTO loginRequestDTO) {
+		return userService
+			.findByEmail(loginRequestDTO.getEmail())
+			.filter(u -> passwordEncoder.matches(loginRequestDTO.getPassword(), u.getPassword()))
+			.map(u -> jwtUtils.generateToken(u.getEmail(), u.getRole()));
+	}
+
+}
